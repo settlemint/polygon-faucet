8cb4987807374cc4434db4ea76b932b4dea68dfa
Roderik van der Veer <roderik@settlemint.com>
2021-11-09 00:11:32 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded())
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://matic-faucet-fe.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://matic-faucet-fe.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
